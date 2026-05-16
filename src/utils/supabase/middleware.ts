120f366ced67119eb633ca6f78fc1f9fc72aa78a
Tetsuro Wada <tetsurou@tron-lab.com>
2026-05-16 11:16:23 +0900

    ローカルで立ち上げたときの不具体対応

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -20,7 +20,10 @@ export async function updateSession(request: NextRequest) {
             request,
           });
           cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options),
+            supabaseResponse.cookies.set(name, value, {
+              ...options,
+              secure: process.env.NODE_ENV === 'production',
+            }),
           );
         },
       },
